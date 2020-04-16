aa8219095c282e011a91b44948e0412027af0a44
Gustavo Peixoto <henriquegustavo2010@gmail.com>
2020-04-16 12:53:31 -0300

    Criacao da classe Worker parte 1 - Criacao as auto proprieties

---
+++ Enumeracao_Exer_Resol/Entities/Worker.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using Enumeracao_Exer_Resol.Entities.Enums;
+
+namespace Enumeracao_Exer_Resol.Entities
+{
+    class Worker
+    {
+        public string Name { get; set; }
+        public WorkLevel Level { get; set; }
+        public double BaseSalary { get; set; }
+        public Department Department { get; set; }
+        public List<HourContract> Contracts { get; private set; } = new List<HourContract>();
+    }
+}
+
+
