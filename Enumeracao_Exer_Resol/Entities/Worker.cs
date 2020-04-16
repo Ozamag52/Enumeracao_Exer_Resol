cd7f3558e4fdc2fe55795a3fc1ad6d8442b482dc
Gustavo Peixoto <henriquegustavo2010@gmail.com>
2020-04-16 13:33:02 -0300

    Criacao da classe Worker parte 2 - Criacao dos construtores, e todos os metodos

--- Enumeracao_Exer_Resol/Entities/Worker.cs
+++ Enumeracao_Exer_Resol/Entities/Worker.cs
@@ -11,6 +11,52 @@ namespace Enumeracao_Exer_Resol.Entities
         public double BaseSalary { get; set; }
         public Department Department { get; set; }
         public List<HourContract> Contracts { get; private set; } = new List<HourContract>();
+
+
+        public Worker() // Construtor padrão
+        {
+
+        }
+
+        public Worker(string name, WorkLevel level, double baseSalary, Department department)
+        {
+            Name = name;
+            Level = level;
+            BaseSalary = baseSalary;
+            Department = department;
+
+        }
+
+
+        //Criação de metódos
+
+        public void AddContract(HourContract contract) // Metodo responsavel por adicionar um item di tipo HourContract a lista Contracts
+        {
+            Contracts.Add(contract);
+
+        }
+
+        public void RemoveContract(HourContract contract)
+        {
+            Contracts.Remove(contract);
+
+        }
+
+        public double Income(int year, int month)
+        {
+            double sum = BaseSalary;
+
+            foreach(HourContract contract in Contracts)
+            {
+                if(contract.Date.Year == year && contract.Date.Month == month)
+                {
+                    sum += contract.TotalValue();
+                }
+            }
+
+            return sum;
+        }
+
     }
 }
 
