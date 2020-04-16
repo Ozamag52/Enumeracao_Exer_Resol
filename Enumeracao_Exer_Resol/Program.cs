dac2142bdf94de0c5b652060de7eefcc38805e8f
Gustavo Peixoto <henriquegustavo2010@gmail.com>
2020-04-16 12:03:44 -0300

    Versão 0

---
+++ Enumeracao_Exer_Resol/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Enumeracao_Exer_Resol
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
