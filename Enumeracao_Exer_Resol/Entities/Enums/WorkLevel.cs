9b834c91941235a03d216ac40f9077d050679ebf
Gustavo Peixoto <henriquegustavo2010@gmail.com>
2020-04-16 12:38:20 -0300

    Criacao da enumeracao WorkLevel

---
+++ Enumeracao_Exer_Resol/Entities/Enums/WorkLevel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Enumeracao_Exer_Resol.Entities.Enums
+{
+    enum WorkLevel : int 
+    {
+        Junior = 0,
+        MidLevel = 1,
+        Senior = 2
+    }
+}
